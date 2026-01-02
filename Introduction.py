ef9100983b12ded51a68a12cd213b6d329f8bac3
Sibasish Padhihari <sibasishpadhihari@gmail.com>
2026-01-02 15:07:56 +0530

    Add Introduction.py with basic operations and types.

---
+++ Introduction.py
@@ -0,0 +1,21 @@
+# Introduction
+print(3 + 2)   # addition(+)
+print(3 - 2)   # subtraction(-)
+print(3 * 2)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# data types
+
+print(type(10))                  # Int type
+print(type(3.14))                # Float type
+print(type(1 + 3j))              # Complex type 
+print(type('Asabeneh'))          # String type
+print(type([1, 2, 3]))           # List type  
+print(type({'name':'Asabeneh'})) # Dictionary type
+print(type({9.8, 3.14, 2.7}))    # Set type 
+print(type((9.8, 3.14, 2.7)))    # Tuple type
+print(type(3 == 3))              # Bool type
+print(type(3 >= 3))              # Bool type
